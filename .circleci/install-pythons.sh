1f2a118ffbf8f7704fe9c9583f05b695ff9a0141
Jessamyn Smith <geekchick77@gmail.com>
2015-04-20 15:32:07 -0400

    Change wget options to install pythonz correctly

--- .circleci/install-pythons.sh
+++ .circleci/install-pythons.sh
@@ -4,7 +4,7 @@ set -e
 BUILDROOT=$HOME/heartbot
 
 if [[ ! -e .pythonz ]]; then
-    wget https://raw.github.com/saghul/pythonz/master/pythonz-install | bash
+    wget -O - https://raw.github.com/saghul/pythonz/master/pythonz-install | bash
 
     declare -a pythons=("2.6.9" "2.7.9" "3.2.6", "3.3.6", "3.4.2")
     for i in "${pythons[@]}"

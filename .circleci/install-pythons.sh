595b67a6d1e6a81628a441739dd4cfe471f6e0ff
Jessamyn Smith <geekchick77@gmail.com>
2015-04-20 15:51:03 -0400

    Use only pythonz-supported versions

--- .circleci/install-pythons.sh
+++ .circleci/install-pythons.sh
@@ -7,7 +7,7 @@ export PYTHONZ_ROOT=$BUILDROOT/.pythonz
 if [[ ! -e .pythonz ]]; then
     wget -O - https://raw.github.com/saghul/pythonz/master/pythonz-install | bash
 
-    declare -a pythons=("2.6.9" "2.7.9" "3.2.6", "3.3.6", "3.4.2")
+    declare -a pythons=("2.6.9" "2.7.9" "3.2.5", "3.3.5", "3.4.2")
     for i in "${pythons[@]}"
     do
         $BUILDROOT/.pythonz/bin/pythonz install "$i"

39ed6fd3747e11abc21240f13055de173247adb3
Jessamyn Smith <geekchick77@gmail.com>
2015-04-20 15:19:44 -0400

    Try using pythonz for python versions

---
+++ .circleci/install-pythons.sh
@@ -0,0 +1,17 @@
+set -x
+set -e
+
+BUILDROOT=$HOME
+#BUILDROOT=$HOME/heartbot
+
+if [[ ! -e .pythonz ]]; then
+    wget https://raw.github.com/saghul/pythonz/master/pythonz-install | bash
+
+    declare -a pythons=("2.6.9" "2.7.9" "3.2.6", "3.3.6", "3.4.2")
+    for i in "${pythons[@]}"
+    do
+        $BUILDROOT/.pythonz/bin/pythonz install "$i"
+        ln -s "$BUILDROOT/.pythonz/pythons/CPython-$i/bin/python${i:0:3}" ~/bin/
+    done
+
+fi

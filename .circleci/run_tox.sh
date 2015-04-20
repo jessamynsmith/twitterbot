30157e1c029e96440294b5ab3f0d6f822713de42
Jessamyn Smith <geekchick77@gmail.com>
2015-04-20 17:09:14 -0400

    Try using a script to run tox

---
+++ .circleci/run_tox.sh
@@ -0,0 +1,6 @@
+export PATH=/home/ubuntu/.pyenv/versions/2.6.8/bin:$PATH
+export PATH=/home/ubuntu/.pyenv/versions/2.7.9/bin:$PATH
+export PATH=/home/ubuntu/.pyenv/versions/3.2.5/bin:$PATH
+export PATH=/home/ubuntu/.pyenv/versions/3.3.3/bin:$PATH
+export PATH=/home/ubuntu/.pyenv/versions/3.4.2/bin:$PATH
+tox

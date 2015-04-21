b8e8cb636bfb7372f82dc47319877d3588392627
Jessamyn Smith <geekchick77@gmail.com>
2015-04-20 23:53:36 -0400

    Dynamically set up python versions for tox

--- .circleci/run_tox.sh
+++ .circleci/run_tox.sh
@@ -1,6 +1,11 @@
-export PATH=/home/ubuntu/.pyenv/versions/2.6.8/bin:$PATH
-export PATH=/home/ubuntu/.pyenv/versions/2.7.9/bin:$PATH
-export PATH=/home/ubuntu/.pyenv/versions/3.2.5/bin:$PATH
-export PATH=/home/ubuntu/.pyenv/versions/3.3.3/bin:$PATH
-export PATH=/home/ubuntu/.pyenv/versions/3.4.2/bin:$PATH
+#!/bin/bash
+
+# This script will quit on the first error that is encountered.
+set -e
+
+for version in "$@"
+do
+    export PATH="$HOME/.pyenv/versions/$version/bin":$PATH
+done
+
 tox

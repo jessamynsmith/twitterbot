124308a38d8ad31db0dae0e1ec7a367b5df0a6d6
jessamynsmith <jessamyn.smith@gmail.com>
2019-05-31 12:17:09 -0600

    Updated to version 0.3.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os.path as p
 
 
-version = "0.3.3"
+version = "0.3.4"
 
 with open(p.join(p.dirname(__file__), 'requirements', 'package.txt'), 'r') as reqs:
     install_requires = [line.strip() for line in reqs]
